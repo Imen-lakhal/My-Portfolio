c59acdfc6d9b20e303eda1db136fa694f059aa29
Imen lakhal <imen.lakhal@ensi-uma.tn>
2024-10-24 20:31:24 +0100

    last commit

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 const express = require("express");
 const cors = require("cors");
-
+require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -9,19 +9,21 @@ app.use(express.json());
 const port = 8000;
 
 const con = mysql.createConnection({
-  user: "root",
+  user: process.env.DB_USER,           
   host: "localhost",
-  password: "",
-  database: "portfolio",
+  password: process.env.DB_PASSWORD,   
+  database: process.env.DB_NAME,       
 });
 
-con.connect(function (err) {
+con.connect((err) => {
   if (err) {
-    console.log("Error in connection");
-  } else {
-    console.log("Connected");
+    console.error("Error in connection:", err.code); // Affiche le code d'erreur
+    console.error("Error message:", err.message);    // Affiche le message d'erreur
+    return;
   }
+  console.log("Connected to MySQL database.");
 });
+
 
 app.get("/", (req, res) => {
   res.send("Hello !");
